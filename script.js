6c850fe4d6ac2c40ea20eb086d4b19840fce51c7
Prazial <robertprazial@gmail.com>
2026-04-21 16:50:22 +0530

    Completing the Project

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ let operator = "";
 const currentDisplayNumber = document.querySelector(".currentNumber");
 const previousDisplayNumber = document.querySelector(".previousNumber");
 
-const numberButtons = document.querySelectorAll(".btn:not(.operator):not(.allclear):not(.clear):not(.equals)");
+
+window.addEventListener("keydown", handelKeyboardInput);
+
+const numberButtons = document.querySelectorAll(".btn:not(.operator):not(.allclear):not(.clear):not(.equals):not(.decimal)");
 
 const operators = document.querySelectorAll(".btn.operator");
 
+const decimals = document.querySelector(".btn.decimal");
+decimals.addEventListener("click", () => {
+ addDecimal();
+});
 const equals = document.querySelector(".btn.equals");
 equals.addEventListener("click", () => {
   if (previousNum != "" && currentNum != "") {
@@ -20,6 +27,7 @@ const clear = document.querySelector(".btn.allclear");
 clear.addEventListener("click", clearCalculator); 
 
 const deleteBtn = document.querySelector(".btn.clear");
+deleteBtn.addEventListener("click", handleDelete);
 
 numberButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -46,11 +54,21 @@ operators.forEach((btn) => {
 });
 
 function handleOperator(op) {
-    operator = op;
+  if (previousNum !== "" && currentNum !== "") {
+    calculate();
+  }
+
+  if (previousNum === "") {
     previousNum = currentNum;
-    currentNum = "";
-    previousDisplayNumber.textContent = previousNum + " " + operator;
-    currentDisplayNumber.textContent = currentNum;
+  }
+  operatorCheck(op);
+}
+
+function operatorCheck(text) {
+  operator = text;
+  previousDisplayNumber.textContent = previousNum + " " + operator;
+  currentDisplayNumber.textContent = "0";
+  currentNum = "";
 }
 
 function calculate() {
@@ -91,6 +109,7 @@ function displayResults(result) {
   previousDisplayNumber.textContent = "";
   operator = "";
   currentNum = "";
+  previousNum = result;
 }
 
 function clearCalculator() {
@@ -99,4 +118,44 @@ function clearCalculator() {
   operator = "";
   currentDisplayNumber.textContent = "0";
   previousDisplayNumber.textContent = "";
+}
+
+function addDecimal() {
+  if (!currentNum.includes(".")) {
+    currentNum += ".";
+    currentDisplayNumber.textContent = currentNum;
+  }
+}
+
+function handelKeyboardInput(e) {
+    e.preventDefault();
+  if (e.key >= 0 && e.key <= 9) {
+    handleNumber(e.key); 
+  }if (e.key === "Enter" || e.key === "="  && previousNum != "" && currentNum != "") { 
+    calculate();
+  }if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === "%") {
+    let op = e.key;
+    handleOperator(op);
+  } else if (e.key === "Backspace") {
+    handleDelete();
+  } else if (e.key === "Escape") {
+    clear.click();
+  } else if (e.key === ".") {
+    addDecimal();
+  }
+
+}
+
+function handleDelete() {
+  if (currentNum !== "") {
+    currentNum = currentNum.slice(0, -1);
+    currentDisplayNumber.textContent = currentNum;
+    if (currentNum === "") {
+      currentDisplayNumber.textContent = "0";
+    }
+  }
+  if (currentNum === "" && previousNum !== "" && operator === "") {
+    previousNum = previousNum.slice(0, -1);
+    currentDisplayNumber.textContent = previousNum;
+  }
 }
